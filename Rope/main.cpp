d9884b8f5342680d68ee14207e4649ca7a884b99
PhillNo <24880693+PhillNo@users.noreply.github.com>
2023-07-29 14:34:51 -0700

    main.cpp: Clarified documentation/comments

--- Rope/main.cpp
+++ Rope/main.cpp
@@ -11,6 +11,10 @@ int main()
     auto buff1 = std::span<const char>(new char[12]{'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'}, 12); 
     auto buff3 = std::span<const char>(new char[10]{'B','r','a','v','e',' ','N','e','w',' '}, 10); 
     auto buff4 = std::span<const char>(new char[1]{'*'}, 1);
+    auto buff5 = std::span<const char>(new char[1]{' '}, 1);
+
+    cout << "Single character insertions at indices [0,n] of a \"monolithic\" Rope:\n"
+        "============================================================================" << endl;
 
     for (int i = 0; i < 23; i++)
     {
@@ -20,36 +24,49 @@ int main()
         phillno::Rope<const char> star_rope(buff4);
         R1.insert(i, star_rope); //insert rope
 
-        cout << "*" << R1 << "*" << endl << flush;        
+        cout << R1 << endl << flush;        
     }
 
+
+    cout << "\n\nRemove single character from \"monolithic\" Rope:\n"
+        "============================================================================" << endl;
+
     phillno::Rope<const char> R1(buff1);
-    cout << "\n*" << R1 << "*" << endl;
+    cout << R1 << endl;
 
     R1.remove(5, 1);
-    cout << "*" << R1 << "*" << endl;
+    cout << R1 << endl;
 
-    R1 = phillno::Rope<const char>(buff1); //test copy constructor
-    cout << "*" << R1 << "*" << endl;
+
+    cout << "\n\nInstantiating via copy contstructor:\n"
+        "============================================================================" << endl;
+
+    R1 = phillno::Rope<const char>(R1);
+    cout << R1 << endl;
+
+
+    cout << "\n\nInserting Rope into Rope:\n"
+        "============================================================================" << endl;
     auto R3 = phillno::Rope<const char>(buff3);
+    R1.insert(5, phillno::Rope<const char>(buff5));
     R1.insert(6, R3);
-    cout << "*" << R1 << "*" << endl;
+    
+    cout << R1 << endl;
 
-    //ensure no side effects in R1 due to changes in R3
+
+    cout << "\n\nInserting Ropes at ends of previously used Rope:\n"
+        "============================================================================" << endl;
     auto R4 = phillno::Rope<const char>(buff4);
-    R3.insert(0, R4);
-    R3.insert(R3.length(), R4);
-    
-    cout << "*" << R1 << "*" << endl;
+    R1.insert(0, R4);
+    R1.insert(R1.length(), R4);
+    cout << R1 << endl;
 
+
+    cout << "\n\nRemoving multiple characters from \"composite\" Rope:\n"
+        "============================================================================" << endl;
     R1.remove(6, 10);
-    cout << "*" << R1 << "*\n" << endl;
+    cout << R1 << endl;
     
-    //phillno::Rope<int> R5 {};// = phillno::Rope<int>(); // Rope<int> R5(); is not a constructor!
-    //cout << "*" << R5 << "*" << endl;
-    //int r5 = R5.at(0);
-    //cout << "*" << r5 << "*" << endl;
-
 
     return 0;
 
